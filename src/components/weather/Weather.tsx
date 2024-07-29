27ac121eb87c79d5d0ca5535f3b5dbcc18b120b8
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-07-29 14:45:23 +0500

    repaired weather design

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,7 +1,21 @@
+// CSS
 import "./Weather.css";
 
+// Icon
+import sun from "../../assets/icons/sun.png";
+
 function Weather() {
-  return <div>Weather</div>;
+  return (
+    <div className="weather flex alignCenter">
+      <img src={sun} alt="Sun Icon" />
+      <div className="flex column">
+        <span className="celsius">31°C</span>
+        <div className="region">
+          <h3>Urgench, UZ</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
