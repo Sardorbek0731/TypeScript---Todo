67c6a385147f146e128ecb2399cdc6c6bb34b23f
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-08-02 00:26:01 +0500

    repaired clock

--- src/components/time/Time.tsx
+++ src/components/time/Time.tsx
@@ -1,13 +1,43 @@
 // CSS
 import "./Time.css";
 
+// Hooks
+import { useState } from "react";
+
 function Time(): JSX.Element {
+  let time: string = `${
+    new Date().getHours() >= 10
+      ? new Date().getHours()
+      : "0" + new Date().getHours()
+  }:${
+    new Date().getMinutes() >= 10
+      ? new Date().getMinutes()
+      : "0" + new Date().getMinutes()
+  }`;
+
+  const [clock, setClock] = useState<string>(time);
+
+  const updateTime = (): void => {
+    time = `${
+      new Date().getHours() >= 10
+        ? new Date().getHours()
+        : "0" + new Date().getHours()
+    }:${
+      new Date().getMinutes() >= 10
+        ? new Date().getMinutes()
+        : "0" + new Date().getMinutes()
+    }`;
+    setClock(time);
+  };
+
+  setInterval(updateTime, 1000);
+
   return (
     <div className="time flex justifyBetween wp-100">
       <div className="title">
         <h1>Note</h1>
       </div>
-      <h3>16:20</h3>
+      <h3>{clock}</h3>
     </div>
   );
 }
