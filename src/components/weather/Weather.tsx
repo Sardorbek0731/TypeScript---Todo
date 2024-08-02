0ef1305182108db41ab40c26a1848018bb0a340f
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-08-02 23:10:11 +0500

    repaired weather icon mistake

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -12,13 +12,17 @@ function Weather(): JSX.Element | null {
     "http://ip-api.com/json/?fields=61439"
   );
 
+  data && console.log(data);
+
   return (
     data && (
       <div className="weather todoItem flex alignCenter">
-        <img
-          src={`http://openweathermap.org/img/wn/${data[1].weather[0].icon}.png`}
-          alt="Sun Icon"
-        />
+        <div className="weatherIcon flex justifyCenter alignCenter">
+          <img
+            src={`http://openweathermap.org/img/wn/${data[1].weather[0].icon}.png`}
+            alt="Weather Icon"
+          />
+        </div>
         <span className="flex column">
           <h1 className="celsius">{Math.round(data[1].main.temp)}°</h1>
           <h3 className="region">
