0e8b05d4f3054ce22111d7ed4cc08f6a771de720
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-08-03 17:33:02 +0500

    changed design

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,10 +1,15 @@
 // CSS
 import "./Input.css";
 
+// Image
+import feather from "../../assets/images/feather/feather.png";
+
 function Input(): JSX.Element {
   return (
     <div className="input wp-100">
-      <input type="text" placeholder="Type your todos ..." autoComplete="off" />
+      <input type="text" id="input" autoComplete="off" />
+      <label htmlFor="input">Type your todos ...</label>
+      <img src={feather} alt="Feather Image" />
     </div>
   );
 }
